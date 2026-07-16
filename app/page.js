17fac010751609589abdf6a66346639e3b41b6c7
Rondale Rae Bufete <108667491+rondale-bufete@users.noreply.github.com>
2026-07-16 21:46:57 +0800

    +: vercel/speed-insights for vercel deployment speed insights

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
